ba058432963b38287606640833a239c23ee2be2e
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-01-31 09:40:24 -0300

    Criado componente Post, ajustado imagens e layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 
 import './global.css'
 import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
@@ -10,9 +11,9 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-        <aside>
+        <Sidebar>
           Sidebar
-        </aside>
+        </Sidebar>
           <main>
             <Post
               author="Rafael Ribeiro"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,27 @@
-export function Sidebar(){
-    
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/RaffahOlive.png" />
+                <strong>Rafael Ribeiro</strong>
+                <span>Software Engineer</span>
+            </div>
+
+            <footer>
+                <a href='#'>
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+
+    )
 }
