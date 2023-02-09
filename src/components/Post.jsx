952d1943d6d714bbcca68b1993aa7795e83dbb86
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-02-09 10:14:29 -0300

    Criado componente de Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { Comment } from './Comment'
+import { Avatar } from './Avatar'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -6,10 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img
-                        className={styles.avatar}
-                        src="https://github.com/RaffahOlive.png"
-                    />
+                   <Avatar src="https://github.com/RaffahOlive.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Rafael Ribeiro</strong>
                         <span>Software Engineer</span>
